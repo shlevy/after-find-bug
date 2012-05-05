6a20ec43d0f3b840c4cc9260fc3931fc48a067c4
Shea Levy <shea@shealevy.com>
2012-05-05 16:50:06 -0400

    TimeLog and Milestone are InvoiceableModels

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,4 +1,8 @@
 class TimeLog < ActiveRecord::Base
+  include InvoiceableModel
+  after_find :set_invoice
+  before_save :set_billing_invoice
+
   belongs_to :project
   belongs_to :invoice
   attr_accessible :billing_invoice
