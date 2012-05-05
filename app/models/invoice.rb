511c43063005cae986dd2b9e5d6c2d365a0d5e9d
Shea Levy <shea@shealevy.com>
2012-05-05 16:46:01 -0400

    Set up has_many associations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,5 @@
 class Invoice < ActiveRecord::Base
+  has_many :milestones
+  has_many :time_logs
   attr_accessible :number
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :milestones
+  has_many :time_logs
   attr_accessible :name
 end
