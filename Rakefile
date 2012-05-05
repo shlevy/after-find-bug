fc4b521aa5d665c181e825788bec38a95e23f6d6
Shea Levy <shea@shealevy.com>
2012-05-05 16:38:42 -0400

    rails new after-find-bug

---
+++ Rakefile
@@ -0,0 +1,7 @@
+#!/nix/store/f7xb8wf7z9f264hsq06bm589wkxrzchw-coreutils-8.15/bin/env rake
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+AfterFindBug::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AfterFindBug::Application.config.secret_token = '7bc30a8576544cfac620ab4337dd63bccf0d6d66a6779eab48dc8eb22e62560f018d9d3288f18ed2c5c1fc9f857482cd28d3d780c5b13ac0975e19abb2c2daf3'
